d5a845b1419a27f9522bc1ff0750b34173460b93
Dawid Tomaszewski <dawidtomaszewski672@gmail.com>
2024-09-26 23:05:40 +0200

    Struktura Controllera

--- Creators/Creators.Controllers/PhotoController.cs
+++ Creators/Creators.Controllers/PhotoController.cs
@@ -1,1 +1,89 @@
-﻿
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Creators.Creators.Controllers
+{
+    public class PhotoController : Controller
+    {
+        [HttpGet]
+        public async Task<IActionResult> ManagePhotos()
+        {
+            //przez usera
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> CheckLikes()
+        {
+
+        }
+        [HttpGet]
+        public async Task<IActionResult> CheckComments()
+        {
+
+        }
+
+
+
+        [HttpPost]
+        public async Task<IActionResult> AddPhoto(string Description, bool CommentsOpen, IFormFile file)
+        {
+            //obsługa
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> DeletePhoto(int Id)
+        {
+            //obsługa
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> CretorPhotos(string Id_Photos) // do zarządzania swojmi zdjeciami
+        {
+            //obsługa
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> UserCreatorsPhotos() // wielu twórców
+        {
+            //obsługa
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> UserCreatorPhotos() // jednego twórców
+        {
+            //obsługa
+        }
+
+
+
+        [HttpPost]
+        public async Task<IActionResult> LikePhoto(string HeartGroup)
+        {
+
+        }
+        [HttpPost]
+
+        public async Task<IActionResult> UnLinkePhoto(int Id)
+        {
+
+        }
+
+
+        [HttpPost]
+        public async Task<IActionResult> AddComment(string CommentsGroup)
+        {
+
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> DeleteComment(int Id)
+        {
+
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> ChangeVisibleComment(int Id)
+        {
+
+        }
+    }
+}
